f886330c641d63d8a9bf6582e54b36782a0da476
Jasha Joachimsthal <jasha@onegini.com>
2015-01-28 11:32:46 +0100

    Fix syntax error in handling failed login

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -27,6 +27,13 @@ module.exports = [
         "clobbers": [
             "cordova.logger"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -34,7 +41,8 @@ module.exports.metadata =
 {
     "org.apache.cordova.device": "0.2.13",
     "org.apache.cordova.dialogs": "0.2.11",
-    "org.apache.cordova.console": "0.2.12"
+    "org.apache.cordova.console": "0.2.12",
+    "org.apache.cordova.statusbar": "0.1.9"
 }
 // BOTTOM OF METADATA
 });

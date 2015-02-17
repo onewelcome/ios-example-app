15c1022052bdd8edfc115df23bde7905138aaff0
Pawel Lewandowski <pawel@onegini.com>
2015-02-17 11:54:59 +0100

    plugin structure changes
    Former-commit-id: fc419286bb77ca2dbb3240ea888eb468c47f24d1

---
+++ CordovaLib/build/CordovaLib.build/Debug-iphonesimulator/CordovaLib.build/Objects-normal/i386/CDVTimer.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/assessment/IdeaProjects/cordova-plugin/platforms/ios/CordovaLib/Classes/CDVTimer.m \
+  /Users/assessment/IdeaProjects/cordova-plugin/platforms/ios/CordovaLib/Classes/CDVTimer.h

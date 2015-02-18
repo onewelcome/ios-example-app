12543f41e111d521de647dc68b10201774c73081
Jasha Joachimsthal <jasha@onegini.com>
2015-02-18 16:25:44 +0100

    MS-164 Test setup
    Former-commit-id: d62a5070fc746b359efa576d91a81e58984130e9

---
+++ tests/tests.js
@@ -0,0 +1,7 @@
+exports.defineAutoTests = function () {
+  describe('Onegini Cordova Plugin basics', function () {
+    it('is defined', function () {
+      expect(oneginiCordovaPlugin).toBeDefined(1);
+    });
+  });
+};

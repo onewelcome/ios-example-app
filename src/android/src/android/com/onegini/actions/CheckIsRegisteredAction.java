3fd10ac0a5ab96800279cae50fe0f115db568051
Pawel Lewandowski <pawel@onegini.com>
2015-03-20 08:23:58 +0100

    MS-238, removed javadocs
    Former-commit-id: 570562b318bb2d306c2d385ca466c4792c0d8a8d

--- src/android/src/android/com/onegini/actions/CheckIsRegisteredAction.java
+++ src/android/src/android/com/onegini/actions/CheckIsRegisteredAction.java
@@ -5,11 +5,7 @@ import org.json.JSONArray;
 
 import com.onegini.OneginiCordovaPlugin;
 
-/**
- * Created by Robert on 19.03.15.
- */
 public class CheckIsRegisteredAction implements OneginiPluginAction {
-
   @Override
   public void execute(final JSONArray args, final CallbackContext callbackContext, final OneginiCordovaPlugin client) {
     if (client.getOneginiClient().isRegistered()) {

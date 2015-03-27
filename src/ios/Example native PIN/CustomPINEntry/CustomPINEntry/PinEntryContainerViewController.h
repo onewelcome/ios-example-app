5f33d35e23a72d80b42277cbf8af51e550992ab4
Eduard Mossinkoff <eduard@EMoss-MBP.local>
2015-03-27 15:55:29 +0100

    Added simple custom configuration (JSON).
    Former-commit-id: 4ef9b15b96e3cc667f51a518bf69b146a43a115c

--- src/ios/Example native PIN/CustomPINEntry/CustomPINEntry/PinEntryContainerViewController.h
+++ src/ios/Example native PIN/CustomPINEntry/CustomPINEntry/PinEntryContainerViewController.h
@@ -9,15 +9,30 @@
 #import <UIKit/UIKit.h>
 #import "PinEntryViewController.h"
 
+/*
+ Customization keys
+ */
+
+/** Full size backgound image */
+extern NSString *kBackgoundImage;
+extern NSString *kKeyColor;
+
 /**
  This view provides the container for the PIN entry component. The PIN entry component will only be capturing 
- the user input. This container provides other user interaction components like result message dialogs and 
- help screens.
+ the user input. This container maintains state and provides other user interaction components like result message dialogs and
+ help screens. The visual representation has limited customization properties.
  */
  
 @interface PinEntryContainerViewController : UIViewController <PinEntryViewControllerDelegate>
 
 @property (strong, nonatomic) PinEntryViewController *pinEntryViewController;
 @property (weak, nonatomic) IBOutlet UIView *pinViewPlaceholder;
+@property (weak, nonatomic) IBOutlet UIImageView *containerBackgroundImage;
 
+/**
+ Apply custom style 
+ 
+ @param config
+ */
+- (void)applyConfig:(NSDictionary *)config;
 @end

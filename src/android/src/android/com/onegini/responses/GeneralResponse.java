b39b5df6bf5dad2cb3c77bb3fab06844bbc2a191
Pawel Lewandowski <pawel@onegini.com>
2015-04-14 15:30:56 +0200

    MS-346,  unsupportedAppVersion  in plugin initialization flow, needs server API to be finished
    Former-commit-id: 540ea471e3c45a853e681b1f0d8789ec111617a9

--- src/android/src/android/com/onegini/responses/GeneralResponse.java
+++ src/android/src/android/com/onegini/responses/GeneralResponse.java
@@ -2,8 +2,9 @@ package com.onegini.responses;
 
 public enum GeneralResponse {
 
-  NO_INTERNET_CONNECTION("noInternetConnection");
-  
+  NO_INTERNET_CONNECTION("noInternetConnection"),
+  UNSUPPORTED_APP_VERSION("unsupportedAppVersion");
+
   private final String name;
 
   GeneralResponse(final String name) {

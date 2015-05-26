48f22b68be629b3cae97b263ba6c2450b956842d
Stanisław Brzeski <sbrzeski@MacBook-Pro-Stanisaw.local>
2015-05-26 14:49:08 +0200

    Pop message on fin forgotten
    Former-commit-id: 592b27c989109411e0d6bd9a1f26c40e8dc3ef12

--- src/ios/OneginiCordovaPlugin/Classes/PopupViewController.m
+++ src/ios/OneginiCordovaPlugin/Classes/PopupViewController.m
@@ -9,6 +9,7 @@
 #import "PopupViewController.h"
 
 @interface PopupViewController ()
+@property (weak, nonatomic) IBOutlet UIView *labelView;
 
 @end
 
@@ -25,5 +26,22 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)setPopupMessage:(NSString*)message{
+    float textHeight = self.contentTextView.frame.size.height;
+    self.contentTextView.text = message;
+    CGRect labrect = [message boundingRectWithSize:self.contentTextView.frame.size options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:self.contentTextView.font} context:Nil];
+    
+    self.view.frame = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y, self.view.frame.size.width, self.view.frame.size.height-(textHeight-labrect.size.height-25));`
+}
+
+- (IBAction)buttonProceedClick:(id)sender {
+    self.proceedBlock();
+}
+- (IBAction)buttonCancelClick:(id)sender {
+    self.cancelBlock();
+}
+- (IBAction)buttonCloseClick:(id)sender {
+    self.closeBlock();
+}
 
 @end

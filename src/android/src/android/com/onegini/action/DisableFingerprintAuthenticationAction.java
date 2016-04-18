2a80abda597587daf29b12ca0601b43299a22898
Patryk Roszczyniala <patryk@onegini.com>
2016-04-18 09:01:52 +0200

    CORDOPLUG-19; callback sent after disabling fingerprint

--- src/android/src/android/com/onegini/action/DisableFingerprintAuthenticationAction.java
+++ src/android/src/android/com/onegini/action/DisableFingerprintAuthenticationAction.java
@@ -5,6 +5,7 @@ import org.json.JSONArray;
 
 import com.onegini.OneginiCordovaPlugin;
 import com.onegini.mobile.sdk.android.library.OneginiClient;
+import com.onegini.util.CallbackResultBuilder;
 
 public class DisableFingerprintAuthenticationAction implements OneginiPluginAction {
 
@@ -12,6 +13,8 @@ public class DisableFingerprintAuthenticationAction implements OneginiPluginActi
   public void execute(final JSONArray args, final CallbackContext callbackContext, final OneginiCordovaPlugin client) {
     final OneginiClient oneginiClient = client.getOneginiClient();
     oneginiClient.disableFingerprintAuthentication();
+
+    callbackContext.sendPluginResult(new CallbackResultBuilder().withSuccess().build());
   }
 
 }

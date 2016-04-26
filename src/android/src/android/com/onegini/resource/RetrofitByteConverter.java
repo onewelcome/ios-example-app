edbc5b013ffe7a8abaa85d3cf20560827c9f21a2
Patryk Roszczyniala <patryk@onegini.com>
2016-04-26 11:22:28 +0200

    CORDOPLUG-32; retrofit byte converter reimplemented so it works for all type of objects (previously it worked only for strings)

--- src/android/src/android/com/onegini/resource/RetrofitByteConverter.java
+++ src/android/src/android/com/onegini/resource/RetrofitByteConverter.java
@@ -1,11 +1,14 @@
 package com.onegini.resource;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.lang.reflect.Type;
 
 import org.apache.commons.io.IOUtils;
 
-import com.onegini.mobile.sdk.android.library.helpers.TypedBodyConverter;
 import retrofit.converter.ConversionException;
 import retrofit.converter.Converter;
 import retrofit.mime.TypedInput;
@@ -20,7 +23,12 @@ public class RetrofitByteConverter implements Converter {
 
   @Override
   public TypedOutput toBody(final Object object) {
-    return new TypedBodyConverter().toBody(object);
+    try {
+      final byte[] bytes = getBytes(object);
+      return new ByteTypedOutput(bytes);
+    } catch (IOException e) {
+      return new ByteTypedOutput(new byte[0]);
+    }
   }
 
   public static byte[] fromTypedInput(final TypedInput input) {
@@ -28,6 +36,42 @@ public class RetrofitByteConverter implements Converter {
       return IOUtils.toByteArray(input.in());
     } catch (IOException e) {
       return new byte[0];
+    }
+  }
+
+  private byte[] getBytes(final Object object) throws IOException {
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    ObjectOutput out = new ObjectOutputStream(bos);
+    out.writeObject(object);
+    return bos.toByteArray();
+  }
+
+  private static class ByteTypedOutput implements TypedOutput {
+
+    private final byte[] bytes;
+
+    public ByteTypedOutput(final byte[] bytes) {
+      this.bytes = bytes;
+    }
+
+    @Override
+    public String fileName() {
+      return null;
+    }
+
+    @Override
+    public String mimeType() {
+      return "";
+    }
+
+    @Override
+    public long length() {
+      return bytes.length;
+    }
+
+    @Override
+    public void writeTo(final OutputStream out) throws IOException {
+      out.write(bytes);
     }
   }
 

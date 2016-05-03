24a2db5fb4bb1a18fd64c19f25dd713880ec0da4
Serhii Butenko <sereja.butenko@gmail.com>
2016-05-03 12:25:57 +0300

    [EXAMPLEIOS-2] Add empty project

---
+++ OneginiDemoApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OneginiDemoApp
+//
+//  Created by Sergey Butenko on 3/5/16.
+//  Copyright © 2016 Onegini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OneginiDemoApp/AppDelegate.m
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.m
+//  OneginiDemoApp
+//
+//  Created by Sergey Butenko on 3/5/16.
+//  Copyright © 2016 Onegini. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@end

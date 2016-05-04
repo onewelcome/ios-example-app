aa0918d9b519e22152771f5d5b11f91b01ff0cee
Serhii Butenko <sereja.butenko@gmail.com>
2016-05-04 12:16:53 +0300

    EXAMPLEIOS-3 Add certificate for Onegini Client

---
+++ OneginiDemoApp/Misk/Constants.h
@@ -0,0 +1,11 @@
+//
+//  Constants.h
+//  OneginiDemoApp
+//
+//  Created by Sergey Butenko on 4/5/16.
+//  Copyright © 2016 Onegini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXTERN NSString *const OneginiCertificate;

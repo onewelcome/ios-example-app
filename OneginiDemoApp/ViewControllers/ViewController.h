fcca2c06925facc4d275c8e41b53173d29be6697
Serhii Butenko <sereja.butenko@gmail.com>
2016-05-04 09:51:27 +0300

    EXAMPLEIOS-3 Add basic view controller

---
+++ OneginiDemoApp/ViewControllers/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OneginiDemoApp
+//
+//  Created by Sergey Butenko on 3/5/16.
+//  Copyright © 2016 Onegini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ OneginiDemoApp/ViewControllers/ViewController.m
@@ -0,0 +1,17 @@
+//
+//  ViewController.m
+//  OneginiDemoApp
+//
+//  Created by Sergey Butenko on 3/5/16.
+//  Copyright © 2016 Onegini. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (IBAction)login:(id)sender {
+    
+}
+
+@end

1ede2a0c8085f7749042bd5f3e4f7be463fb5edb
zen <zen.dev@yandex.com>
2016-06-30 18:59:07 +0300

    Temporary dummy imp of enrollment

--- OneginiDemoApp/ViewControllers/ProfileViewController.m
+++ OneginiDemoApp/ViewControllers/ProfileViewController.m
@@ -11,14 +11,19 @@
 #import "LogoutController.h"
 #import "DisconnectController.h"
 #import "Profile.h"
+#import "OneginiSDK.h"
+#import "PinViewController.h"
+#import "AppDelegate.h"
 
-@interface ProfileViewController()
+@interface ProfileViewController() <OGFingerprintDelegate, OGEnrollmentHandlerDelegate>
 
 @property (weak, nonatomic) IBOutlet UIView *profileDataView;
 @property (weak, nonatomic) IBOutlet UILabel *profileMailLabel;
 @property (weak, nonatomic) IBOutlet UILabel *profileNameLabel;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *getProfileSpinner;
 @property (weak, nonatomic) IBOutlet UIButton *getProfileButton;
+@property (weak, nonatomic) IBOutlet UIButton *mobileEnrollmentButton;
+@property (weak, nonatomic) IBOutlet UIButton *fingerprintEnrollmentButton;
 
 @end
 
@@ -28,6 +33,8 @@
     [super viewDidLoad];
     self.profileDataView.hidden = YES;
     self.getProfileSpinner.hidden = YES;
+    
+    [self updateEnrollmentVisibleState];
 }
 
 -(void)viewDidAppear:(BOOL)animated{
@@ -57,6 +64,31 @@
     }];
 
 }
+- (IBAction)enableMobileEnrollment:(id)sender {
+    [[OGOneginiClient sharedInstance] enrollForMobileAuthentication:@[@"defailt"] delegate:self];
+}
+
+- (void)updateEnrollmentVisibleState {
+    OGOneginiClient *client = [OGOneginiClient sharedInstance];
+        
+    self.fingerprintEnrollmentButton.enabled = [client isFingerprintAuthenticationAvailable];
+    NSString *fingerprintTitle = [client isFingerprintAuthenticationAvailable] ? [client isEnrolledForFingerprintAuthentication] ? @"Enrolled" : @"Enroll" : @"TouchID N/A";
+    [self.fingerprintEnrollmentButton setTitle:fingerprintTitle forState:UIControlStateNormal];
+}
+
+- (IBAction)switchFingerprintEnrollment:(id)sender {
+    OGOneginiClient *client = [OGOneginiClient sharedInstance];
+    if (!client.isFingerprintAuthenticationAvailable) {
+        return;
+    }
+    
+    if ([client isEnrolledForFingerprintAuthentication]) {
+        [client disableFingerprintAuthentication];
+        [self updateEnrollmentVisibleState];
+    } else {
+        [client enrollForFingerprintAuthentication:@[@"default"] delegate:self];
+    }
+}
 
 - (void)showError:(NSString*)error{
     UIAlertController * alert = [UIAlertController alertControllerWithTitle:@"Resource error"
@@ -67,6 +99,76 @@
                                                handler:^(UIAlertAction * action){}];
     [alert addAction:ok];
     [self presentViewController:alert animated:YES completion:nil];
+}
+
+- (void) askCurrentPinForFingerprintAuthentication {
+    PinViewController *viewController = [PinViewController new];
+    viewController.pinLength = 5;
+    viewController.mode = PINCheckMode;
+    viewController.pinEntered = ^(NSString *pin) {
+        [[OGOneginiClient sharedInstance] confirmCurrentPin:pin];
+    };
+    [[AppDelegate sharedNavigationController] pushViewController:viewController animated:YES];
+}
+
+- (void) fingerprintAuthenticationEnrollmentSuccessful {
+    [self updateEnrollmentVisibleState];
+}
+
+- (void) fingerprintAuthenticationEnrollmentFailure {
+    [self showError:NSStringFromSelector(_cmd)];
+}
+
+- (void) fingerprintAuthenticationEnrollmentFailureNotAuthenticated {
+    [self showError:NSStringFromSelector(_cmd)];
+}
+
+- (void) fingerprintAuthenticationEnrollmentErrorInvalidPin:(NSUInteger)attemptCount {
+    [self showError:NSStringFromSelector(_cmd)];
+}
+
+- (void) fingerprintAuthenticationEnrollmentErrorTooManyPinFailures {
+    [self showError:NSStringFromSelector(_cmd)];
+}
+
+- (void)enrollmentSuccess {
+    [self updateEnrollmentVisibleState];
+}
+
+- (void)enrollmentError {
+    [self showError:NSStringFromSelector(_cmd)];
+}
+
+- (void)enrollmentAuthenticationError {
+    [self showError:NSStringFromSelector(_cmd)];
+}
+
+- (void)enrollmentError:(NSError *)error {
+    [self showError:NSStringFromSelector(_cmd)];
+}
+
+- (void)enrollmentNotAvailable {
+    [self showError:NSStringFromSelector(_cmd)];
+}
+
+- (void)enrollmentInvalidRequest {
+    [self showError:NSStringFromSelector(_cmd)];
+}
+
+- (void)enrollmentInvalidClientCredentials {
+    [self showError:NSStringFromSelector(_cmd)];
+}
+
+- (void)enrollmentDeviceAlreadyEnrolled {
+    [self showError:NSStringFromSelector(_cmd)];
+}
+
+- (void)enrollmentUserAlreadyEnrolled {
+    [self showError:NSStringFromSelector(_cmd)];
+}
+
+- (void)enrollmentInvalidTransaction {
+    [self showError:NSStringFromSelector(_cmd)];
 }
 
 @end

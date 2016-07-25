dfc4bb1d2e0abe3353a698e6538351066a3ac17c
zen <zen.dev@yandex.com>
2016-07-25 12:07:37 +0300

    SDKIOS-170 Updated SDK headers

---
+++ OneginiSDKiOS/Headers/ONGFingerprintDelegate.h
@@ -0,0 +1,52 @@
+//  Copyright (c) 2016 Onegini. All rights reserved.
+
+#import <Foundation/Foundation.h>
+#import "ONGPinConfirmation.h"
+#import "ONGUserProfile.h"
+
+/**
+ *  Delegate used for fingerprint authentication.
+ */
+@protocol ONGFingerprintDelegate<NSObject>
+
+/**
+ *  Asks user for current pin for fingerprint authentication enrollment for a specific profile.
+ *  Call the ONGPinConfirmation - (void)confirmPin:(NSString *)pin method; with the user provided PIN.
+ */
+- (void)askCurrentPinForFingerprintEnrollmentForUser:(ONGUserProfile *)userProfile pinConfirmation:(id<ONGPinConfirmation>)pinConfirmation;
+
+/**
+ *  Fingerprint enrollment success callback.
+ */
+- (void)fingerprintAuthenticationEnrollmentSuccessful;
+
+/**
+ *  Fingerprint enrollment failure callback.
+ */
+- (void)fingerprintAuthenticationEnrollmentFailure;
+
+/**
+ *  Fingerprint enrollment failure due to invalid client credentials, which means client is disconnected.
+ */
+- (void)fingerprintAuthenticationEnrollmentFailureNotAuthenticated;
+
+/**
+ *  Fingerprint enrollment failure due to invalid pin.
+ *
+ *  @param attemptCount number of available attempts
+ */
+- (void)fingerprintAuthenticationEnrollmentErrorInvalidPin:(NSUInteger)attemptCount;
+
+/**
+ *  Error occurred during the fingerprint enrollment request, all data for the current profile was removed. This user needs to register again.
+ *  This can happen when the profile is removed server-side or the user tried to enter the PIN too many times.
+ */
+- (void)fingerprintAuthenticationEnrollmentErrorUserDeregistered;
+
+/**
+ *  Fingerprint enrollment failed. All device data including all profiles were removed. The user needs to register again.
+ *  This can happen when the device registration is removed server-side.
+ */
+- (void)fingerprintAuthenticationEnrollmentErrorDeviceDeregistered;
+
+@end

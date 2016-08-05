007d37da632f84c692f8999051f17826c9afd412
Stein Welberg <stein@onegini.com>
2016-08-05 14:50:05 +0200

    SDKIOS-102 Listen to errors that can happen when calling the start method on the Onegini SDK

--- OneginiDemoApp/AppDelegate.m
+++ OneginiDemoApp/AppDelegate.m
@@ -3,7 +3,6 @@
 #import "AppDelegate.h"
 #import "WelcomeViewController.h"
 #import "MobileAuthenticationController.h"
-#import "ONGClientBuilder.h"
 
 @implementation AppDelegate
 
@@ -33,9 +32,19 @@
     [self.window makeKeyAndVisible];
 
     [[[[ONGClientBuilder new] setUseEmbeddedWebView:YES] setStoreCookies:YES] build];
-    
+
     [[ONGClientBuilder new] build];
-    [[ONGClient sharedInstance] start:nil];
+    [[ONGClient sharedInstance] start:^(BOOL result, NSError *error) {
+        if (error != nil) {
+            if (ONGGenericErrorOutdatedApplication == error.code) {
+                [self showAlertWithTitle:@"Application disabled" message:@"The application version is no longer valid, please visit the app store to update your application"];
+            }
+
+            if (ONGGenericErrorOutdatedOS == error.code) {
+                [self showAlertWithTitle:@"OS outdated" message:@"The operating system that you use is no longer valid, please update your OS."];
+            }
+        }
+    }];
 
     UIUserNotificationType supportedTypes = UIUserNotificationTypeAlert | UIUserNotificationTypeBadge | UIUserNotificationTypeSound;
     UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:supportedTypes categories:nil];
@@ -46,17 +55,31 @@
     return YES;
 }
 
-- (void)application:(UIApplication*)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+- (void)showAlertWithTitle:(NSString *)title message:(NSString *)message
+{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title
+                                                                   message:message
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *okButton = [UIAlertAction
+        actionWithTitle:@"Ok"
+                  style:UIAlertActionStyleDefault
+                handler:^(UIAlertAction *action) {
+                }];
+    [alert addAction:okButton];
+    [[AppDelegate sharedNavigationController] presentViewController:alert animated:YES completion:nil];
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 {
     [[ONGUserClient sharedInstance] storeDevicePushTokenInSession:deviceToken];
 }
 
-- (void)application:(UIApplication*)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
 {
     [[ONGUserClient sharedInstance] storeDevicePushTokenInSession:nil];
 }
 
--(void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
 {
     [[ONGUserClient sharedInstance] handlePushNotification:userInfo delegate:[MobileAuthenticationController sharedInstance]];
 }

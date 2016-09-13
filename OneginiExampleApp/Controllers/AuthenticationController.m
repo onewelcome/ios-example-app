95294fd106b1dad828d2b5434f294d59fc4084df
zen <zen.dev@yandex.com>
2016-09-13 14:08:27 +0300

    EXAMPLEIOS-21 Add authentication controller errors comments

--- OneginiExampleApp/Controllers/AuthenticationController.m
+++ OneginiExampleApp/Controllers/AuthenticationController.m
@@ -3,6 +3,7 @@
 #import "AuthenticationController.h"
 #import "PinViewController.h"
 #import "ProfileViewController.h"
+#import "PinErrorMapper.h"
 
 @interface AuthenticationController ()
 
@@ -33,7 +34,15 @@
 
 - (void)userClient:(ONGUserClient *)userClient didFailToAuthenticateUser:(ONGUserProfile *)userProfile error:(NSError *)error
 {
-    [self.navigationController popToRootViewControllerAnimated:YES];
+    // In case User has enter invalid PIN too many times (configured on the Token Server), SDK automatically deregisters User.
+    // Developer at this point has to "logout" UI, shutdown user-related services and display Authorization.
+    if (error.code == ONGGenericErrorUserDeregistered || error.code == ONGGenericErrorDeviceDeregistered) {
+        [self.navigationController popToRootViewControllerAnimated:YES];
+    } else if (error.code == ONGGenericErrorActionCancelled) {
+        // If challenge has been cancelled than ONGGenericErrorActionCancelled is reported.
+        // In the example app authorization is done in the root of the application navigation stack, thats why we're simply popping pin view controller
+        [self.navigationController popToRootViewControllerAnimated:YES];
+    }
 
     [self showError:error];
 
@@ -51,10 +60,16 @@
         [challenge.sender respondWithPin:pin challenge:challenge];
     };
 
-    if (challenge.previousFailureCount > 0) {
-        [self.pinViewController showError:[NSString stringWithFormat:@"Wrong Pin. Remaining attempts: %@", @(challenge.remainingFailureCount)]];
-    } else {
+    // It is up to the developer to decide when and how to show PIN entry view controller.
+    // For simplicity of the example app we're checking the top-most view controller.
+    if (![self.navigationController.topViewController isEqual:self.pinViewController]) {
         [self.navigationController pushViewController:self.pinViewController animated:YES];
+    }
+
+    if (challenge.error) {
+        // Please read comments for the PinErrorMapper to understand intent of this class and how errors can be handled.
+        NSString *description = [PinErrorMapper descriptionForError:challenge.error ofPinChallenge:challenge];
+        [self.pinViewController showError:description];
     }
 }
 

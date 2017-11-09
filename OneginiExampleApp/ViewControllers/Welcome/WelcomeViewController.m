6df4ed4889c7127eaeafd0b73ca569fecfbaefa6
Łukasz Łabuński <lukasz@onegini.com>
2017-11-09 17:38:04 +0100

    SDKIOS-415 added error code in new lane

--- OneginiExampleApp/ViewControllers/Welcome/WelcomeViewController.m
+++ OneginiExampleApp/ViewControllers/Welcome/WelcomeViewController.m
@@ -235,7 +235,7 @@
     ONGResourceRequest *request = [[ONGResourceRequest alloc] initWithPath:@"resources/user-id-decorated" method:@"GET"];
     [[ONGUserClient sharedInstance] fetchImplicitResource:request completion:^(ONGResourceResponse *_Nullable response, NSError *_Nullable error) {
         if (error) {
-            self.profileLabel.text = @"Fetching implicit resource failed";
+            self.profileLabel.text = [NSString stringWithFormat:@"Fetching implicit resource failed \r %ld", error.code];
         } else {
             id jsonResponse = [response JSONResponse];
             if (jsonResponse != nil) {

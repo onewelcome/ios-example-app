720937699550714d62affd5637abad484540bb37
Łukasz Łabuński <lukasz@onegini.com>
2017-11-27 13:21:30 +0100

    EXAMPLEIOS-40 added Expire time to transaction

--- OneginiExampleApp/ViewControllers/Transactions/PendingTransactionTableViewCell.m
+++ OneginiExampleApp/ViewControllers/Transactions/PendingTransactionTableViewCell.m
@@ -11,6 +11,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *profileNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *messageLabel;
 @property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *expireTime;
 
 @end
 
@@ -20,6 +21,7 @@
 {
     self.profileNameLabel.text = [[ProfileModel new] profileNameForUserProfile:pendingTransaction.userProfile];
     self.timeLabel.text = [self convertDateToString:pendingTransaction.date];
+    self.expireTime.text = [NSString stringWithFormat:@"Will expire at: %@", [self convertDateToString:[self addingTimeInterval:pendingTransaction.timeToLive date:pendingTransaction.date]]];
     self.messageLabel.text = pendingTransaction.message;
 }
 
@@ -28,6 +30,11 @@
     return [NSDateFormatter localizedStringFromDate:date
                                         dateStyle:NSDateFormatterNoStyle
                                         timeStyle:NSDateFormatterMediumStyle];
+}
+
+- (NSDate *)addingTimeInterval:(NSNumber *)timeInterval date:(NSDate *)date
+{
+    return [date dateByAddingTimeInterval:([timeInterval doubleValue])];
 }
 
 @end
